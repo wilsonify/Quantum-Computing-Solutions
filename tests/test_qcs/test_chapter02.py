4c3751fe7316782f1d48d4954dc0743a437264f5
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-04-03 21:41:12 -0400

    create tests

---
+++ tests/test_qcs/test_chapter02.py
@@ -0,0 +1,11 @@
+from qcs import chapter2
+from qcs.chapter2 import (
+    set_operations,
+    matrix_operations,
+    tensor_operations,
+    quantum_operations
+)
+
+
+def test_smoke():
+    print(dir(chapter2))
